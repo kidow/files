da34a2ea76139809c538702eaeed704d2f9b9fe7
kidow <wcgo2ling@gmail.com>
2022-09-25 11:04:31 +0900

    경로명 통일

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.use('/image', express.static('images'))
+app.use(express.static('image'))
 
 app.listen(4000, () => {
   console.log(`Example app listening on port 4000`)
