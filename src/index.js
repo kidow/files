140381fb8ca025f942a7f07e770bf9006d0bc2c6
kidow <wcgo2ling@gmail.com>
2022-09-25 12:43:57 +0900

    4줄로 축소

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 const express = require('express')
-
 const app = express()
-
 app.use(express.static('image'))
-
-app.listen(4000, () => {
-  console.log(`Example app listening on port 4000`)
-})
+app.listen(4000)
